3e59c0cf7e4f2a9450452f270757517f4ca93385
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2025-04-20 23:57:05 -0300

    feat: add Segurado class

--- src/br/edu/cs/poo/ac/seguro/entidades/Segurado.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Segurado.java
@@ -1,5 +1,61 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
-public class Segurado {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+public class Segurado {
+	private String nome;
+	private Endereco endereco;
+	private LocalDate dataCriacao;
+	private BigDecimal bonus;
+	
+	public Segurado(String nome, Endereco endereco, LocalDate dataCriacao, 
+					BigDecimal bonus) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.dataCriacao = dataCriacao;
+		this.bonus = bonus;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	protected LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	protected void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	public BigDecimal getBonus() {
+		return bonus;
+	}
+	
+	
+	public int getIdade() {
+		return (int) ChronoUnit.YEARS.between(dataCriacao, LocalDate.now());
+	}
+	
+	public void creditarBonus(BigDecimal valor) {
+		bonus = bonus.add(valor);
+	}
+	
+	public void debitarBonus(BigDecimal valor) {
+		bonus = bonus.subtract(valor);
+	}
 }
