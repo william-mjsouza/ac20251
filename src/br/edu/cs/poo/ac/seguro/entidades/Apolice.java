c08e942dc7d5a7e12d5231540db9d7d8f03a6bb4
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2025-04-28 09:01:43 -0300

    feat: add atribute numero in Apolice class

--- src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -5,11 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 
 public class Apolice {
+	private String numero;
 	private Veiculo veiculo;
 	private BigDecimal valorFranquia;
 	private BigDecimal valorPremio;
 	private BigDecimal valorMaximoSegurado;
+	
+	public Apolice(Veiculo veiculo, BigDecimal valorFranquia, BigDecimal valorPremio, BigDecimal valorMaximoSegurado) {
+		super();
+		this.veiculo = veiculo;
+		this.valorFranquia = valorFranquia;
+		this.valorPremio = valorPremio;
+		this.valorMaximoSegurado = valorMaximoSegurado;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
 }
