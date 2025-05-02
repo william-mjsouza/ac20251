2cfbdef848e8a8ab386ef719ba5641d30a725afa
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2025-05-02 15:58:30 -0300

    feat: add methods ehNuloOuBranco and temSomenteNumeros

--- src/br/edu/cs/poo/ac/seguro/mediators/StringUtils.java
+++ src/br/edu/cs/poo/ac/seguro/mediators/StringUtils.java
@@ -3,9 +3,20 @@ package br.edu.cs.poo.ac.seguro.mediators;
 public class StringUtils {
 	private StringUtils() {}
 	public static boolean ehNuloOuBranco(String str) {
-		return false;
+		return (str == null) || str.trim().isEmpty();
 	}
+	
     public static boolean temSomenteNumeros(String input) {
-        return false; 
+    	if (ehNuloOuBranco(input)) {
+            return false;
+        }
+    	
+        for (int i = 0; i < input.length(); i++) {
+        	if (!(input.charAt(i) >= '0'  && input.charAt(i) <= '9')) {
+        		return false;
+        	}
+        }
+        
+    	return true; 
     }
 }
