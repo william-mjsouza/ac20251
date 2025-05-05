c6069d577e0b139d2455d8f8e729a9952f7d2f90
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2025-05-05 17:49:41 -0300

    feat: add SeguradoMediator methods

--- src/br/edu/cs/poo/ac/seguro/mediators/SeguradoPessoaMediator.java
+++ src/br/edu/cs/poo/ac/seguro/mediators/SeguradoPessoaMediator.java
@@ -16,28 +16,81 @@ public class SeguradoPessoaMediator {
     }
 	
 	public String validarCpf(String cpf) {
-		return null;
+		if (StringUtils.ehNuloOuBranco(cpf)) {
+            return "CPF deve ser informado";
+        }
+		
+        String digitos = cpf.replaceAll("\\D", "");
+        if (digitos.length() != 11) {
+            return "CPF deve ter 11 caracteres";
+        }
+        
+        if (!ValidadorCpfCnpj.ehCpfValido(cpf)) {
+            return "CPF com dígito inválido";
+        }
+        
+        return null;
 	}
 	public String validarRenda(double renda) {
-		return null;
+		if (renda < 0.0) {
+            return "Renda deve ser maior ou igual à zero";
+        }
+		
+        return null;
 	}
 	public String incluirSeguradoPessoa(SeguradoPessoa seg) {
-		// Acho que tem que implementar essa lógica com base no teste
-		validarSeguradoPessoa(seg);
-		return null;
+		String msg = validarSeguradoPessoa(seg);
+        if (msg != null) return msg;
+        
+        if (seguradoPessoaDAO.buscar(seg.getCpf()) != null) {
+            return "CPF do segurado pessoa já existente";
+        }
+        seguradoPessoaDAO.incluir(seg);
+        
+        return null;
 	}
 	public String alterarSeguradoPessoa(SeguradoPessoa seg) {
-		// Acho que tem que implementar essa lógica com base no teste
-		validarSeguradoPessoa(seg);
-		return null;
+		String msg = validarSeguradoPessoa(seg);
+        if (msg != null) return msg;
+        
+        if (seguradoPessoaDAO.buscar(seg.getCpf()) == null) {
+            return "CPF do segurado pessoa não existente";
+        }
+        seguradoPessoaDAO.alterar(seg);
+        
+        return null;
 	}
 	public String excluirSeguradoPessoa(String cpf) {
-		return null;
+		if (seguradoPessoaDAO.buscar(cpf) == null) {
+            return "CPF do segurado pessoa não existente";
+        }
+		seguradoPessoaDAO.excluir(cpf);
+		
+        return null;
 	}
 	public SeguradoPessoa buscarSeguradoPessoa(String cpf) {
-		return null;
+		return seguradoPessoaDAO.buscar(cpf);
 	}
 	public String validarSeguradoPessoa(SeguradoPessoa seg) {
-		return null;
+
+        String msg = seguradoMediator.validarNome(seg.getNome());
+        if (msg != null) return msg;
+        
+        msg = seguradoMediator.validarEndereco(seg.getEndereco());
+        if (msg != null) return msg;
+
+        if (seg.getDataNascimento() == null) {
+            return "Data do nascimento deve ser informada";
+        }
+        msg = seguradoMediator.validarDataCriacao(seg.getDataNascimento());
+        if (msg != null) {
+
+            return msg.replace("criação", "nascimento");
+        }
+ 
+        msg = validarCpf(seg.getCpf());
+        if (msg != null) return msg;
+        
+        return validarRenda(seg.getRenda());
 	}
 }
