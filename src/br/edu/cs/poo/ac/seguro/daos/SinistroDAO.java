640a6b6f9b216ab33bcf148103b10fa8310c4991
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-05-05 22:39:59 -0300

    SinistroDAO

--- src/br/edu/cs/poo/ac/seguro/daos/SinistroDAO.java
+++ src/br/edu/cs/poo/ac/seguro/daos/SinistroDAO.java
@@ -1,53 +1,53 @@
 package br.edu.cs.poo.ac.seguro.daos;
 
 import java.io.Serializable;
-
+import java.util.ArrayList;
+import java.util.List;
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 import br.edu.cs.poo.ac.seguro.entidades.Sinistro;
 
 public class SinistroDAO extends DAOGenerico {
-
-	public SinistroDAO() {
-		cadastro = new CadastroObjetos(Sinistro.class);
-	}
-
-	public Sinistro buscar(String numero) {
-		return (Sinistro) cadastro.buscar(numero);
-	}
-
-	public boolean incluir(Sinistro sinistro) {
-		if (buscar(sinistro.getNumero()) != null) {
-			return false;
-		} else {
-			cadastro.incluir((Serializable) sinistro, sinistro.getNumero());
-			return true;
-		}
-	}
-
-	public boolean alterar(Sinistro sinistro) {
-		if (buscar(sinistro.getNumero()) == null) {
-			return false;
-		} else {
-			cadastro.alterar((Serializable) sinistro, sinistro.getNumero());
-			return true;
-		}
-	}
-
-	public boolean excluir(String numero) {
-		if (buscar(numero) == null) {
-			return false;
-		} else {
-			cadastro.excluir(numero);
-			return true;
-		}
-	}
-	
-	public Sinistro[] buscarTodos() {
-        Object[] objs = cadastro.buscarTodos();
-        Sinistro[] result = new Sinistro[objs.length];
-        for (int i = 0; i < objs.length; i++) {
-            result[i] = (Sinistro) objs[i];
+    
+    public SinistroDAO() {
+        cadastro = new CadastroObjetos(Sinistro.class);
+    }
+    public List<Sinistro> buscarTodos() {
+        Serializable[] obj = cadastro.buscarTodos();
+        List<Sinistro> list = new ArrayList<>();
+        for (Serializable objs : obj) {
+            list.add((Sinistro) objs);
         }
-        return result;
+        return list;
+    }
+    
+    public Sinistro buscar(String numero) {
+        return (Sinistro) cadastro.buscar(numero);
+    }
+    
+    public boolean incluir(Sinistro sinistro) {
+        if (buscar(sinistro.getNumero()) != null) {
+            return false;
+        } else {
+            cadastro.incluir((Serializable) sinistro, sinistro.getNumero());
+            return true;
+        }
+    }
+    
+    public boolean alterar(Sinistro sinistro) {
+        if (buscar(sinistro.getNumero()) == null) {
+            return false;
+        } else {
+            cadastro.alterar((Serializable) sinistro, sinistro.getNumero());
+            return true;
+        }
+    }
+    
+    public boolean excluir(String numero) {
+        if (buscar(numero) == null) {
+            return false;
+        } else {
+            cadastro.excluir(numero);
+            return true;
+        }
     }
 }
