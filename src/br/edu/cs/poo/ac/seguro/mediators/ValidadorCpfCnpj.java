04f644d584364334204443ac5403ba287c7b68db
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:25:13 -0300

    Att ValidadosCpfCnpj.java

--- src/br/edu/cs/poo/ac/seguro/mediators/ValidadorCpfCnpj.java
+++ src/br/edu/cs/poo/ac/seguro/mediators/ValidadorCpfCnpj.java
@@ -1,55 +1,64 @@
 package br.edu.cs.poo.ac.seguro.mediators;
 
 public class ValidadorCpfCnpj {
-	public static boolean ehCnpjValido(String cnpj) {
-		if (cnpj == null) return false;
 
-        String num = cnpj.replaceAll("\\D", "");
-        if (num.length() != 14 || num.matches("(\\d)\\1{13}")) {
+    public static boolean ehCpfValido(String cpf) {
+        if (cpf == null) return false;
+
+        cpf = cpf.replaceAll("\\D", "");
+
+        if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) return false;
+
+        try {
+            int soma = 0, peso = 10;
+            for (int i = 0; i < 9; i++) {
+                soma += (cpf.charAt(i) - '0') * peso--;
+            }
+            int digito1 = 11 - (soma % 11);
+            digito1 = (digito1 > 9) ? 0 : digito1;
+
+            soma = 0;
+            peso = 11;
+            for (int i = 0; i < 10; i++) {
+                soma += (cpf.charAt(i) - '0') * peso--;
+            }
+            int digito2 = 11 - (soma % 11);
+            digito2 = (digito2 > 9) ? 0 : digito2;
+
+            return cpf.charAt(9) - '0' == digito1 && cpf.charAt(10) - '0' == digito2;
+        } catch (Exception e) {
             return false;
         }
+    }
 
-        int[] peso1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
-        int[] peso2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+    public static boolean ehCnpjValido(String cnpj) {
+        if (cnpj == null) return false;
 
-        int soma = 0;
-        for (int i = 0; i < 12; i++) {
-            soma += (num.charAt(i) - '0') * peso1[i];
-        }
-        int resto = soma % 11;
-        int dig1 = (resto < 2) ? 0 : 11 - resto;
+        cnpj = cnpj.replaceAll("\\D", "");
 
-        soma = 0;
-        for (int i = 0; i < 13; i++) {
-            soma += (num.charAt(i) - '0') * peso2[i];
-        }
-        resto = soma % 11;
-        int dig2 = (resto < 2) ? 0 : 11 - resto;
+        if (cnpj.length() != 14 || cnpj.matches("(\\d)\\1{13}")) return false;
 
-        return dig1 == (num.charAt(12) - '0') && dig2 == (num.charAt(13) - '0');
-	}
-	public static boolean ehCpfValido(String cpf) {
-		if (cpf == null) return false;
+        try {
+            int[] pesos1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+            int[] pesos2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
-        String num = cpf.replaceAll("\\D", "");
-        if (num.length() != 11 || num.matches("(\\d)\\1{10}")) {
+            int soma = 0;
+            for (int i = 0; i < 12; i++) {
+                soma += (cnpj.charAt(i) - '0') * pesos1[i];
+            }
+            int digito1 = soma % 11;
+            digito1 = (digito1 < 2) ? 0 : 11 - digito1;
+
+            soma = 0;
+            for (int i = 0; i < 13; i++) {
+                soma += (cnpj.charAt(i) - '0') * pesos2[i];
+            }
+            int digito2 = soma % 11;
+            digito2 = (digito2 < 2) ? 0 : 11 - digito2;
+
+            return cnpj.charAt(12) - '0' == digito1 && cnpj.charAt(13) - '0' == digito2;
+        } catch (Exception e) {
             return false;
         }
-
-        int soma = 0;
-        for (int i = 0; i < 9; i++) {
-            soma += (num.charAt(i) - '0') * (10 - i);
-        }
-        int resto = soma % 11;
-        int dig1 = (resto < 2) ? 0 : 11 - resto;
-
-        soma = 0;
-        for (int i = 0; i < 10; i++) {
-            soma += (num.charAt(i) - '0') * (11 - i);
-        }
-        resto = soma % 11;
-        int dig2 = (resto < 2) ? 0 : 11 - resto;
-
-        return dig1 == (num.charAt(9) - '0') && dig2 == (num.charAt(10) - '0');
-	}
+    }
 }
