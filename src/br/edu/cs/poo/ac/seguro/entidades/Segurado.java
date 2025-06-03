05bdc24d9c587dbc3c76a55963eb7e9688af22a7
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:23:00 -0300

    Att Segurado.java

--- src/br/edu/cs/poo/ac/seguro/entidades/Segurado.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Segurado.java
@@ -1,61 +1,74 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.time.Period;
 
-public class Segurado {
+public abstract class Segurado implements Serializable, Registro {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String nome;
-	private Endereco endereco;
-	private LocalDate dataCriacao;
-	private BigDecimal bonus;
-	
-	public Segurado(String nome, Endereco endereco, LocalDate dataCriacao, 
-					BigDecimal bonus) {
-		this.nome = nome;
-		this.endereco = endereco;
-		this.dataCriacao = dataCriacao;
-		this.bonus = bonus;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public Endereco getEndereco() {
-		return endereco;
-	}
-	
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-	
-	protected LocalDate getDataCriacao() {
-		return dataCriacao;
-	}
-	
-	protected void setDataCriacao(LocalDate dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-	
-	public BigDecimal getBonus() {
-		return bonus;
-	}
-	
-	
-	public int getIdade() {
-		return (int) ChronoUnit.YEARS.between(dataCriacao, LocalDate.now());
-	}
-	
-	public void creditarBonus(BigDecimal valor) {
-		bonus = bonus.add(valor);
-	}
-	
-	public void debitarBonus(BigDecimal valor) {
-		bonus = bonus.subtract(valor);
-	}
+    private Endereco endereco;
+    private LocalDate dataCriacao;
+    private BigDecimal bonus;
+
+    public Segurado(String nome, Endereco endereco, LocalDate dataCriacao, BigDecimal bonus) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.dataCriacao = dataCriacao;
+        this.bonus = bonus;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    protected LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    protected void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public int getIdade() {
+        return Period.between(this.dataCriacao, LocalDate.now()).getYears();
+    }
+
+    public void creditarBonus(BigDecimal valor) {
+        if (valor.compareTo(BigDecimal.ZERO) > 0) {
+            this.bonus = this.bonus.add(valor);
+        }
+    }
+
+    public void debitarBonus(BigDecimal valor) {
+        if (this.bonus.compareTo(valor) >= 0) {
+            this.bonus = this.bonus.subtract(valor);
+        }
+    }
+    
+    public abstract boolean isEmpresa();
+    
+    @Override
+    public abstract String getIdUnico();
 }
