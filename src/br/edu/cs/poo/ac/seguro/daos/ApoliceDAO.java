31df9d989996f0cb184f54b5762c8c6722afdecd
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:20:19 -0300

    att ApoliceDAO.java

--- src/br/edu/cs/poo/ac/seguro/daos/ApoliceDAO.java
+++ src/br/edu/cs/poo/ac/seguro/daos/ApoliceDAO.java
@@ -1,47 +1,17 @@
 package br.edu.cs.poo.ac.seguro.daos;
 
-import java.io.Serializable;
 
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 import br.edu.cs.poo.ac.seguro.entidades.Apolice;
 
-public class ApoliceDAO extends DAOGenerico {
-	
-	public ApoliceDAO() {
-		cadastro = new CadastroObjetos(Apolice.class);
-	}
-	
-	public Apolice buscar(String numero) {
-		return (Apolice)cadastro.buscar(numero);
-	}
-	
-	public boolean incluir(Apolice segurado) {
-		if(buscar(segurado.getNumero()) != null) {
-			return false;
-		}
-		else {
-			cadastro.incluir((Serializable) segurado, segurado.getNumero());
-			return true;
-		}
-	}
-	
-	public boolean alterar(Apolice segurado) {
-		if(buscar(segurado.getNumero()) == null) {
-			return false;
-		}
-		else {
-			cadastro.alterar((Serializable) segurado, segurado.getNumero());
-			return true;
-		}
-	}
-	
-	public boolean excluir(String numero) {
-		if(buscar(numero) == null) {
-			return false;
-		}
-		else {
-			cadastro.excluir(numero);
-			return true;
-		}
-	}
+
+public class ApoliceDAO extends DAOGenerico<Apolice> {
+    
+    public ApoliceDAO() {
+        super();
+    }
+
+    @Override
+    public Class<Apolice> getClasseEntidade() {
+        return Apolice.class;
+    }
 }
