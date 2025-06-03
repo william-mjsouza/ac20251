3f612eae1e3badd9beb024f27417b8e77a7c54c3
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:23:30 -0300

    Att TipoSinistro.java

--- src/br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
@@ -1,29 +1,30 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
 public enum TipoSinistro {
-    COLISAO(1, "Colisão"),
-    INCENDIO(2, "Incêndio"),
+
+    COLISAO(1,"Colisão"),
+    INCENDIO(2,"Incêndio"),
     FURTO(3, "Furto"),
     ENCHENTE(4, "Enchente"),
     DEPREDACAO(5, "Depredação");
 
     private int codigo;
-    private String nome;
+    private String descricao;
 
-    private TipoSinistro(int codigo, String nome) {
+    private TipoSinistro(int codigo, String descricao) {
         this.codigo = codigo;
-        this.nome = nome;
+        this.descricao = descricao;
     }
 
     public int getCodigo() {
         return codigo;
     }
 
-    public String getNome() {
-        return nome;
+    public String getDescricao() {
+        return descricao;
     }
 
-    public static TipoSinistro getTipoSinistro(int codigo) {
+    public static TipoSinistro getTipoSinistro(int codigo){
         for (TipoSinistro tipo : TipoSinistro.values()) {
             if (tipo.getCodigo() == codigo) {
                 return tipo;
