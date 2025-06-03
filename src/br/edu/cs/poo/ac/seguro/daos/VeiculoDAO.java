ab779cca297a5014cdc7f6109c07df8fd4bb83b1
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:21:50 -0300

    Att VeiculoDAO.java

--- src/br/edu/cs/poo/ac/seguro/daos/VeiculoDAO.java
+++ src/br/edu/cs/poo/ac/seguro/daos/VeiculoDAO.java
@@ -1,42 +1,15 @@
 package br.edu.cs.poo.ac.seguro.daos;
 
-import java.io.Serializable;
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 import br.edu.cs.poo.ac.seguro.entidades.Veiculo;
 
-public class VeiculoDAO extends DAOGenerico {
+public class VeiculoDAO extends DAOGenerico<Veiculo> {
+
     public VeiculoDAO() {
-        cadastro = new CadastroObjetos(Veiculo.class);
+        super();
     }
 
-    public Veiculo buscar(String placa) {
-        return (Veiculo) cadastro.buscar(placa);
-    }
-
-    public boolean incluir(Veiculo veiculo) {
-        if (buscar(veiculo.getPlaca()) != null) {
-            return false;
-        } else {
-            cadastro.incluir((Serializable) veiculo, veiculo.getPlaca());
-            return true;
-        }
-    }
-
-    public boolean alterar(Veiculo veiculo) {
-        if (buscar(veiculo.getPlaca()) == null) {
-            return false;
-        } else {
-            cadastro.alterar((Serializable) veiculo, veiculo.getPlaca());
-            return true;
-        }
-    }
-
-    public boolean excluir(String placa) {
-        if (buscar(placa) == null) {
-            return false;
-        } else {
-            cadastro.excluir(placa);
-            return true;
-        }
+    @Override
+    public Class<Veiculo> getClasseEntidade() {
+        return Veiculo.class;
     }
 }
