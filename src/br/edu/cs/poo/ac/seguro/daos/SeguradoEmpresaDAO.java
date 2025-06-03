a1e118c964b7efee546943a9b28a98a2293cbb5d
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:20:43 -0300

    Att SeguradoEmpresaDAO.java

--- src/br/edu/cs/poo/ac/seguro/daos/SeguradoEmpresaDAO.java
+++ src/br/edu/cs/poo/ac/seguro/daos/SeguradoEmpresaDAO.java
@@ -1,46 +1,17 @@
 package br.edu.cs.poo.ac.seguro.daos;
 
-import java.io.Serializable;
 
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 import br.edu.cs.poo.ac.seguro.entidades.SeguradoEmpresa;
 
-public class SeguradoEmpresaDAO extends DAOGenerico {
-	
-	public SeguradoEmpresaDAO() {
-		cadastro = new CadastroObjetos(SeguradoEmpresa.class);
-	}
-	
-	public SeguradoEmpresa buscar(String cnpj) {
-		return (SeguradoEmpresa) cadastro.buscar(cnpj);
-	}
-	
-	public boolean incluir(SeguradoEmpresa segurado) {
-		if(buscar(segurado.getCnpj()) != null) {
-			return false;
-		}
-		else {
-			cadastro.incluir((Serializable) segurado, segurado.getCnpj());
-			return true;
-		}
-	}
-	
-	public boolean alterar(SeguradoEmpresa segurado) {
-		if (buscar(segurado.getCnpj()) == null) {
-			return false;
-		} else {
-			cadastro.alterar((Serializable) segurado, segurado.getCnpj());
-			return true;
-		}
-	}
-	
-	public boolean excluir(String cnpj) {
-		if (buscar(cnpj) == null) {
-			return false;
-		} else {
-			cadastro.excluir(cnpj);
-			return true;
-		}
-	}
-	
+public class SeguradoEmpresaDAO extends SeguradoDAO {
+
+	@Override
+    public Class<SeguradoEmpresa> getClasseEntidade() {
+        return SeguradoEmpresa.class;
+    }
+
+    public SeguradoEmpresa buscar(String id) {
+        return (SeguradoEmpresa) super.buscar(id);
+    }
+
 }
