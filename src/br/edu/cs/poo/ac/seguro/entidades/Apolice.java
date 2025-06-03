f01fda7cad1191b58517a1272fee14e152617a5b
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:21:57 -0300

    Att Apolice.java

--- src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -1,43 +1,25 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
+import lombok.*;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-//import lombok.AllArgsConstructor;
-import lombok.Data;
+@Getter
+@Setter
+@AllArgsConstructor
+public class Apolice implements Serializable,Registro {
 
-@Data
-//@AllArgsConstructor
-
-public class Apolice {
 	private String numero;
-	private Veiculo veiculo;
-	private BigDecimal valorFranquia;
-	private BigDecimal valorPremio;
-	private BigDecimal valorMaximoSegurado;
-	
-	 private LocalDate dataInicioVigencia;
-	
-	public Apolice(String numero, Veiculo veiculo, BigDecimal valorFranquia, 
-				   BigDecimal valorPremio, BigDecimal valorMaximoSegurado, 
-				   LocalDate dataInicioVigencia) {
-		super();
-		this.numero = numero;
-		
-		this.veiculo = veiculo;
-		this.valorFranquia = valorFranquia;
-		this.valorPremio = valorPremio;
-		this.valorMaximoSegurado = valorMaximoSegurado;
-		
-		this.dataInicioVigencia = dataInicioVigencia;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-	
+    private Veiculo veiculo;
+    private BigDecimal valorFranquia;
+    private BigDecimal valorPremio;
+    private BigDecimal valorMaximoSegurado;
+    private LocalDate dataInicioVigencia;
+    
+    @Override
+    public String getIdUnico() {
+        return this.getNumero();
+    }
 }
