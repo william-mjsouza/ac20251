32a8cfd4b1c22a7bcae73f51e876227f9da121c6
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:23:06 -0300

    Att SeguradoEmpresa.java

--- src/br/edu/cs/poo/ac/seguro/entidades/SeguradoEmpresa.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/SeguradoEmpresa.java
@@ -1,52 +1,64 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class SeguradoEmpresa extends Segurado {
-	private String cnpj;
-	private double faturamento;
-	private boolean ehLocadoraDeVeiculos;
-	
-	public SeguradoEmpresa(String nome, Endereco endereco, LocalDate dataAbertura, 
-						   BigDecimal bonus, String cnpj, double faturamento, 
-						   boolean ehLocadoraDeVeiculos) {
-		super(nome, endereco, dataAbertura, bonus);
-		this.cnpj = cnpj;
-		this.faturamento = faturamento;
-		this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
-	}
-	
-	public String getCnpj() {
-		return cnpj;
-	}
-	
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
-	}
-	
-	public double getFaturamento() {
-		return faturamento;
-	}
-	
-	public void setFaturamento(double faturamento) {
-		this.faturamento = faturamento;
-	}
-	
-	public boolean isLocadoraDeVeiculos() {
-		return ehLocadoraDeVeiculos;
-	}
-	
-	public void setLocadoraDeVeiculos(boolean ehLocadoraDeVeiculos) {
-		this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
-	}
-	
-	
-	public LocalDate getDataAbertura() {
-		return getDataCriacao();
-	}
-	
-	public void setDataAbertura(LocalDate dataAbertura) {
-		setDataCriacao(dataAbertura);
-	}
+public class SeguradoEmpresa extends Segurado implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String cnpj;
+    private double faturamento;
+    private boolean ehLocadoraDeVeiculos;
+    
+    public SeguradoEmpresa(String nome, Endereco endereco, LocalDate dataAbertura, 
+                          BigDecimal bonus, String cnpj, double faturamento, 
+                          boolean ehLocadoraDeVeiculos) {
+        super(nome, endereco, dataAbertura, bonus);
+        this.cnpj = cnpj;
+        this.faturamento = faturamento;
+        this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
+    }
+    
+    public String getCnpj() {
+        return cnpj;
+    }
+    
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    public double getFaturamento() {
+        return faturamento;
+    }
+    
+    public void setFaturamento(double faturamento) {
+        this.faturamento = faturamento;
+    }
+    
+    public boolean isEhLocadoraDeVeiculos() {
+        return ehLocadoraDeVeiculos;
+    }
+    
+    public void setEhLocadoraDeVeiculos(boolean ehLocadoraDeVeiculos) {
+        this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
+    }
+    
+    public LocalDate getDataAbertura() {
+        return getDataCriacao();
+    }
+    
+    public void setDataAbertura(LocalDate dataAbertura) {
+        setDataCriacao(dataAbertura);
+    }
+    
+    @Override
+    public boolean isEmpresa() {
+        return true; 
+    }
+    
+    @Override
+    public String getIdUnico() {
+        return this.getCnpj();
+    }
 }
