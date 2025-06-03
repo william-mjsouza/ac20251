d53ecc2d41ac3d53781f8bcdd6a59bd8c1a0cf38
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:20:27 -0300

    att DAOGenerico.java

--- src/br/edu/cs/poo/ac/seguro/daos/DAOGenerico.java
+++ src/br/edu/cs/poo/ac/seguro/daos/DAOGenerico.java
@@ -1,6 +1,63 @@
 package br.edu.cs.poo.ac.seguro.daos;
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 
-public class DAOGenerico {
-	protected CadastroObjetos cadastro;
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import br.edu.cs.poo.ac.seguro.entidades.Registro; 
+import java.io.Serializable; 
+
+public abstract class DAOGenerico<T extends Registro> {
+
+    private CadastroObjetos cadastro;
+
+    public DAOGenerico() {
+        this.cadastro = new CadastroObjetos(getClasseEntidade());
+    }
+
+    public abstract Class<?> getClasseEntidade();
+
+    public boolean incluir(T registro) {
+        if (registro == null || registro.getIdUnico() == null) {
+            return false;
+        }
+        if (buscar(registro.getIdUnico()) != null) {
+            return false; 
+        }
+        cadastro.incluir(registro, registro.getIdUnico());
+        return true;
+    }
+
+    @SuppressWarnings("unchecked") 
+    public T buscar(String id) {
+        if (id == null) {
+            return null;
+        }
+        return (T) cadastro.buscar(id);
+    }
+
+    public boolean alterar(T registro) {
+        if (registro == null || registro.getIdUnico() == null) {
+            return false;
+        }
+        if (buscar(registro.getIdUnico()) == null) {
+            return false; 
+        }
+        cadastro.alterar(registro, registro.getIdUnico());
+        return true;
+    }
+
+    public boolean excluir(String id) {
+        if (id == null || buscar(id) == null) {
+            return false;
+        }
+        cadastro.excluir(id);
+        return true;
+    }
+
+    public Registro[] buscarTodos() {
+        Serializable[] objsSer = cadastro.buscarTodos();
+        Registro[] registros = new Registro[objsSer.length];
+        for (int i = 0; i < objsSer.length; i++) {
+            registros[i] = (Registro) objsSer[i];
+        }
+        return registros;
+    }
 }
