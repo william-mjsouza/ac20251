47a6c4bc025201bcecba1ee978cf5663552d1167
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:26:07 -0300

    Att TesteDAO.java

--- src/br/edu/cs/poo/ac/seguro/testes/TesteDAO.java
+++ src/br/edu/cs/poo/ac/seguro/testes/TesteDAO.java
@@ -1,29 +1,4 @@
 package br.edu.cs.poo.ac.seguro.testes;
 
-import java.io.File;
-
-import org.junit.jupiter.api.BeforeEach;
-
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
-//import br.edu.cs.poo.ac.seguro.daos.SeguradoEmpresaDAO;
-//import br.edu.cs.poo.ac.seguro.entidades.SeguradoEmpresa;
-
 public abstract class TesteDAO extends TesteAbstrato {
-	protected CadastroObjetos cadastro; 
-	
-	protected abstract Class<?> getClasse(); 
-	
-	protected TesteDAO() {
-		cadastro = new CadastroObjetos(getClasse());
-	}
-	
-	@BeforeEach
-	public void setUp() {
-		String sep = File.separator;
-		File dir = new File("." + sep + getClasse().getSimpleName());
-		File[] files = dir.listFiles();
-		for (File file : files) {
-			file.delete();
-		}
-	}
 }
