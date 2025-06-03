a7f1877b7b3f4435ca2a156f85471e75784b817c
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:22:17 -0300

    Att Endereco.java

--- src/br/edu/cs/poo/ac/seguro/entidades/Endereco.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Endereco.java
@@ -1,79 +1,85 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
-public class Endereco {
+import java.io.Serializable;
+
+public class Endereco implements Serializable{
+   
+	private static final long serialVersionUID = 1L;
 	private String logradouro;
-	private String cep;
-	private String numero;
-	private String complemento;
-	private String pais;
-	private String estado;
-	private String cidade;
-	
-	public Endereco(String logradouro, String cep, String numero, String complemento, String pais, String estado,
-			String cidade) {
-		this.logradouro = logradouro;
-		this.cep = cep;
-		this.numero = numero;
-		this.complemento = complemento;
-		this.pais = pais;
-		this.estado = estado;
-		this.cidade = cidade;
-	}
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String pais;
+    private String estado;
+    private String cidade;
 
-	public String getLogradouro() {
-		return logradouro;
-	}
+    public Endereco(String logradouro, String cep, String numero, String complemento, String pais, String estado, String cidade) {
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.pais = pais;
+        this.estado = estado;
+        this.cidade = cidade;
+    }
 
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
+    public String getLogradouro() {
+        return logradouro;
+    }
 
-	public String getCep() {
-		return cep;
-	}
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
 
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
 
-	public String getNumero() {
-		return numero;
-	}
+    public String getCep() {
+        return cep;
+    }
 
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 
-	public String getComplemento() {
-		return complemento;
-	}
+    public String getNumero() {
+        return numero;
+    }
 
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 
-	public String getPais() {
-		return pais;
-	}
+    public String getComplemento() {
+        return complemento;
+    }
 
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
 
-	public String getEstado() {
-		return estado;
-	}
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
 
-	public String getCidade() {
-		return cidade;
-	}
+    public String getPais() {
+        return pais;
+    }
 
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-	
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
 }
