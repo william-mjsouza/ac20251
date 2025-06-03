3ffd053d04316599f6a7f721c50917f6f1a353f6
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:23:13 -0300

    Att SeguradoPessoa.java

--- src/br/edu/cs/poo/ac/seguro/entidades/SeguradoPessoa.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/SeguradoPessoa.java
@@ -1,41 +1,53 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class SeguradoPessoa extends Segurado {
-	private String cpf;
-	private double renda;
-	
-	public SeguradoPessoa(String nome, Endereco endereco, LocalDate dataNascimento, 
-						  BigDecimal bonus, String cpf, double renda) {
-		super(nome, endereco, dataNascimento, bonus);
-		this.cpf = cpf;
-		this.renda = renda;
-	}
-	
-	public String getCpf() {
-		return cpf;
-	}
-	
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public double getRenda() {
-		return renda;
-	}
-	
-	public void setRenda(double renda) {
-		this.renda = renda;
-	}
-	
-	
-	public LocalDate getDataNascimento() {
-		return getDataCriacao();
-	}
-	
-	public void setDataNascimento(LocalDate dataAbertura) {
-		setDataCriacao(dataAbertura);
-	}
+public class SeguradoPessoa extends Segurado implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String cpf;
+    private double renda;
+    
+    public SeguradoPessoa(String nome, Endereco endereco, LocalDate dataNascimento, 
+                         BigDecimal bonus, String cpf, double renda) {
+        super(nome, endereco, dataNascimento, bonus);
+        this.cpf = cpf;
+        this.renda = renda;
+    }
+    
+    public double getRenda() {
+        return renda;
+    }
+    
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+    
+    public LocalDate getDataNascimento() {
+        return getDataCriacao();
+    }
+    
+    public void setDataNascimento(LocalDate dataNascimento) {
+        setDataCriacao(dataNascimento);
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    @Override
+    public boolean isEmpresa() {
+        return false;
+    }
+    
+    @Override
+    public String getIdUnico() {
+    	return this.getCpf();
+    }
 }
