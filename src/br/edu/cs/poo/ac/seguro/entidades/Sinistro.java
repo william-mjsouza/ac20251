da9a9b6df91111651c87fd5a2a8f288c41800d19
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-06-03 13:23:23 -0300

    Att Sinistro.java

--- src/br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
@@ -1,14 +1,17 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-//import lombok.AllArgsConstructor;
+
 import lombok.Data;
+import lombok.AllArgsConstructor;
 
 @Data
-//@AllArgsConstructor
-
-public class Sinistro {
+@AllArgsConstructor
+public class Sinistro implements Serializable, Registro {
+	
+	private static final long serialVersionUID = 1L;
 	private String numero;
 	private Veiculo veiculo;
 	private LocalDateTime dataHoraSinistro;
@@ -16,12 +19,13 @@ public class Sinistro {
 	private String usuarioRegistro;
 	private BigDecimal valorSinistro;
 	private TipoSinistro tipo;
+	private int sequencial;
+	private String numeroApolice;
 	
-	public Sinistro(String numero, Veiculo veiculo, LocalDateTime dataHoraSinistro, 
-					LocalDateTime dataHoraRegistro, String usuarioRegistro, BigDecimal valorSinistro, TipoSinistro tipo) {
+	public Sinistro(String numero, Veiculo veiculo, LocalDateTime dataHoraSinistro, LocalDateTime dataHoraRegistro,
+			String usuarioRegistro, BigDecimal valorSinistro, TipoSinistro tipo) {
 		super();
 		this.numero = numero;
-		
 		this.veiculo = veiculo;
 		this.dataHoraSinistro = dataHoraSinistro;
 		this.dataHoraRegistro = dataHoraRegistro;
@@ -30,13 +34,32 @@ public class Sinistro {
 		this.tipo = tipo;
 	}
 	
+	
 	public String getNumero() {
 		return numero;
 	}
-	
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+	
+	public int getSequencial() {
+		return sequencial;
+	}
+	public void setSequencial(int sequencial) {
+		this.sequencial = sequencial;
+	}
+	
+	public String getNumeroApolice() {
+		return numeroApolice;
+	}
+	public void setNumeroApolice(String numeroApolice) {
+		this.numeroApolice = numeroApolice;
+	}
+	
+	@Override
+    public String getIdUnico() {
+        return this.getNumero(); 
+    }
 	
 	
 }
